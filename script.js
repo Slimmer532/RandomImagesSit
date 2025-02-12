80ced090e81b81e5d0c76fd972b54c591cc09024
Slimmer532 <79303835+Slimmer532@users.noreply.github.com>
2025-02-12 01:57:10 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
-const images = {
-    "League": ["Images/League/image1.jpg", "Images/League/image2.jpg"],
-    "Valorant": ["Images/Valorant/image1.png", "Images/Valorant/image2.png"],
-    "LeagueGIF": ["Images/LeagueGIF/gif1.gif", "Images/LeagueGIF/gif2.gif"],
-    "ValorantGIF": ["Images/ValorantGIF/gif1.gif", "Images/ValorantGIF/gif2.gif"]
-};
+async function fetchImage(category) {
+    const images = {
+        League: ["Images/League/image1.jpg", "Images/League/image2.jpg"],
+        Valorant: ["Images/Valorant/image1.png", "Images/Valorant/image2.png"],
+        LeagueGIF: ["Images/LeagueGIF/gif1.gif", "Images/LeagueGIF/gif2.gif"],
+        ValorantGIF: ["Images/ValorantGIF/gif1.gif", "Images/ValorantGIF/gif2.gif"]
+    };
+
+    // Pick a random image from the selected category
+    const randomIndex = Math.floor(Math.random() * images[category].length);
+    document.getElementById("randomImage").src = images[category][randomIndex];
+}
