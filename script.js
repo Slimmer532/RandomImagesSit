a8eeede252b266f95ec443daeefe9225f604d0f4
Slimmer532 <79303835+Slimmer532@users.noreply.github.com>
2025-02-13 09:57:02 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,76 +1,48 @@
-document.addEventListener("DOMContentLoaded", function () {
-    let currentCategory = "Valorant";
-    let images = [];
-    let preloadedImages = [];
-    let imageIndex = 0;
+let currentCategory = "Valorant";
+let history = [];
+let preloadedImages = [];
 
-    function setCategory(category) {
-        currentCategory = category;
-        images = [];
-        preloadedImages = [];
-        imageIndex = 0;
-        loadImages();
+document.getElementById("invisibleUnlock").addEventListener("click", function() {
+    document.getElementById("aiGirlsButton").style.display = "inline-block";
+});
+
+function setCategory(category) {
+    currentCategory = category;
+    history = [];
+    preloadedImages = [];
+    document.getElementById("randomImage").style.display = "none";
+    document.getElementById("hideButton").style.display = "none";
+    preloadNextImages();
+}
+
+function getRandomImageUrl() {
+    return `https://raw.githubusercontent.com/Slimmer532/RandomImagesSit/main/images/${currentCategory}/` + generateRandomString() + ".jpg";
+}
+
+function generateRandomString() {
+    return Math.random().toString(36).substring(2, 15);
+}
+
+function preloadNextImages() {
+    for (let i = 0; i < 3; i++) {
+        let img = new Image();
+        img.src = getRandomImageUrl();
+        preloadedImages.push(img);
     }
+}
 
-    function loadImages() {
-        fetch(`https://api.github.com/repos/Slimmer532/RandomImagesSit/contents/images/${currentCategory}`)
-            .then(response => response.json())
-            .then(data => {
-                images = data.map(file => file.download_url);
-                preloadNextImages();
-            })
-            .catch(error => console.error("Error loading images:", error));
-    }
-
-    function preloadNextImages() {
-        preloadedImages = images.slice(imageIndex, imageIndex + 3).map(src => {
-            let img = new Image();
-            img.src = src;
-            return img;
-        });
-    }
-
-    function showNextImage() {
-        if (imageIndex >= images.length) imageIndex = 0;
-        document.getElementById("randomImage").src = images[imageIndex];
+function showRandomImage() {
+    if (preloadedImages.length > 0) {
+        let image = preloadedImages.shift();
+        history.push(image.src);
+        document.getElementById("randomImage").src = image.src;
         document.getElementById("randomImage").style.display = "block";
-        imageIndex++;
+        document.getElementById("hideButton").style.display = "block";
         preloadNextImages();
     }
+}
 
-    function toggleImage() {
-        let img = document.getElementById("randomImage");
-        let button = document.getElementById("hideButton");
-        if (img.style.display === "none") {
-            img.style.display = "block";
-            button.innerText = "Hide Image";
-            showNextImage();
-        } else {
-            img.style.display = "none";
-            button.innerText = "Show Image";
-        }
-    }
-
-    document.getElementById("valorantBtn").addEventListener("click", function () {
-        setCategory('Valorant');
-    });
-
-    document.getElementById("leagueBtn").addEventListener("click", function () {
-        setCategory('League');
-    });
-
-    document.getElementById("aiGirlsBtn").addEventListener("click", function () {
-        setCategory('AiGirls');
-    });
-
-    document.getElementById("hideButton").addEventListener("click", function () {
-        toggleImage();
-    });
-
-    document.getElementById("invisibleUnlock").addEventListener("click", function () {
-        document.getElementById("aiGirlsBtn").style.display = "inline-block";
-        alert("AiGirls category unlocked!");
-    });
-
-    setCategory('Valorant');
-});
+function toggleImage() {
+    let img = document.getElementById("randomImage");
+    img.style.display = img.style.display === "none" ? "block" : "none";
+}
